cc7f7634ace568aa712012bc24ced71253d5e6d3
X-EcutiOnner <xex.ecutionner@gmail.com>
2021-01-05 12:57:07 +0700

    Update AssemblyFileVersion

--- Garment Files Generator/Properties/AssemblyInfo.cs
+++ Garment Files Generator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("sader1992@hotmail.com")]
 [assembly: AssemblyProduct("Garment Files Generator")]
-[assembly: AssemblyCopyright("Copyright Sader Fawall ©  2017")]
+[assembly: AssemblyCopyright("Copyright Sader Fawall © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("21.1.5.0")]
+[assembly: AssemblyFileVersion("21.1.5.0")]
